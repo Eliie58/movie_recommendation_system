686fee88471ed766bcf5951422fcff118cb58303
Eliie58 <elies.y@hotmail.com>
2022-11-26 15:21:15 +0100

    Fix linting issues in api code

--- api/Utils.py
+++ api/Utils.py
@@ -1,10 +1,23 @@
+"""
+Module for Utility functions.
+"""
+
 import csv
 import re
+from typing import List
 
 
-def readMovies():
+def read_movies() -> List[object]:
+    """
+    Read movies from resources directory into list of Objects.
+
+    Returns
+    -------
+    list
+        List of movie objects, containing: id, title, year and genre.
+    """
     movies = []
-    with open('api/resources/movies.csv') as file:
+    with open('api/resources/movies.csv', encoding='UTF-8') as file:
         reader = csv.reader(file, )
         # Skip header
         next(reader, None)
