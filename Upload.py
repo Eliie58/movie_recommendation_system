0e051ba3e4040148b7f16d1145dfb389944b6163
RaphSmart <30577625+RaphSmart@users.noreply.github.com>
2022-12-15 20:09:16 +0100

    streamlit upload page updat

---
+++ Upload.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+import json
+from utils.Utils import print_movie_tiles
+import os
+import pandas as pd
+
+api_url = os.environ["API_URL"]
+
+st.set_page_config(
+    page_title="Movie Recommendation System",
+    page_icon='✌️',
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+st.write('## Click to upload files')
+# uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("")
+if uploaded_file is not None:
+
+    dataframe = pd.read_csv(uploaded_file)
+
+    requests.post('http://localhost:8081/upload', {'ids' : dataframe.iloc[:, 0].values } )
+
